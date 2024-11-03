4250d8c7cfa9ed1b7a79ab87e89ca873811bbc8e
TDMwangi <teddymwangi65@gmail.com>
2024-11-03 04:35:57 +0300

    feat: add variable output_path

--- modules/variables.tf
+++ modules/variables.tf
@@ -2,3 +2,8 @@ variable "source_file" {
   description = "File to include in the ZIP archive"
   type        = string
 }
+
+variable "output_path" {
+  description = "The path where the resulting archive file will be saved"
+  type        = string
+}
