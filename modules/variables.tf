4329b0e8b75277ce3e8addc7a1391ca3eb1e0d49
TDMwangi <teddymwangi65@gmail.com>
2024-11-04 05:05:03 +0300

    feat: add variable function_name

--- modules/variables.tf
+++ modules/variables.tf
@@ -7,3 +7,8 @@ variable "output_path" {
   description = "The path where the resulting archive file will be saved"
   type        = string
 }
+
+variable "function_name" {
+  description = "Unique name for the Lambda Function"
+  type        = string
+}
