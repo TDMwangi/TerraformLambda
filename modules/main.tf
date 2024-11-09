bfae5478c2cc90b42297fa26680dbef0f0c386ff
TDMwangi <teddymwangi65@gmail.com>
2024-11-09 04:45:43 +0300

    feat: add principals

--- modules/main.tf
+++ modules/main.tf
@@ -2,5 +2,12 @@
 data "aws_iam_policy_document" "lambda_assume_role_policy" {
   statement {
     effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
   }
 }
