7cc141c23a992e65471d29b4c86f8be5c6f323d7
TDMwangi <teddymwangi65@gmail.com>
2024-11-15 06:40:39 +0300

    feat: attach the policy to the role

--- modules/main.tf
+++ modules/main.tf
@@ -44,3 +44,9 @@ resource "aws_iam_policy" "lambda_iam_policy" {
   name   = "lambda-iam-policy"
   policy = data.aws_iam_policy_document.lambda_iam_policy_document.json
 }
+
+# Attach the policy to the role
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_iam_policy.arn
+}
